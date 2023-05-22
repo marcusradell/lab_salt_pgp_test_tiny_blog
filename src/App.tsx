0c9e72eb5e2be09f1c4f80ff5bcfd70d65eefb37
Marcus Rådell <marcus@radell.net>
2023-05-22 10:14:37 +0200

    remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
 import { data } from "./data";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <pre>{JSON.stringify(data, null, 2)}</pre>
